c666cb6009bbbcb9c3773dc5d49801715459adfa
satyajeet <satyajeetjadhav129@gmail.com>
2023-02-27 12:12:36 +0530

    Commiting Addition file(1st)

---
+++ Addition.java
@@ -0,0 +1,23 @@
+
+
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		System.out.println("Enter Two Numbers For addition");
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter First Number");
+		int num1=sc.nextInt();
+		
+		System.out.println("Enter Second Number");
+		int num2=sc.nextInt();
+		
+		int add=num1+num2;
+		System.out.println("Addition of two numbers is:"+add);
+		sc.close();
+
+	}
+
+}
